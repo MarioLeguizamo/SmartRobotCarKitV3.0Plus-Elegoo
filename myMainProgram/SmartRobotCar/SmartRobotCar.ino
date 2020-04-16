9a18f2edabff22e2ffb285767b5081a4cd031954
Mario Leguizamo <mario.leguizamo@gmail.com>
2020-04-16 00:02:58 -0500

    Continua integración del código de evasión de obstáculos al código del control remoto infrarrojo, se estandarizo y documento el código de myMainProgram

--- myMainProgram/SmartRobotCar/SmartRobotCar.ino
+++ myMainProgram/SmartRobotCar/SmartRobotCar.ino
@@ -1,214 +1,226 @@
+/*************************************LIBRERIAS****************************/
+#include <IRremote.h>       // Libreria control remoto infrarrojo
+#include <Servo.h>          // Libreria servomotor
 
-#include <IRremote.h>   //Librerias
-#include <Servo.h>
+/********************** CODIGOS CONTROL REMOTE INFRARROJO *****************/
+#define F 16736925	          // Adelante
+#define B 16754775	          // Atras
+#define L 16720605	          // Izquierda
+#define R 16761405	          // Derecha
+#define S 16712445	          // Alto
 
-Servo myservo;          //Servo variables
-int Echo = A4;  
-int Trig = A5; 
+#define UNKNOWN_F 5316027		  // Adelante
+#define UNKNOWN_B 2747854299	// Atras
+#define UNKNOWN_L 1386468383	// Izquierda
+#define UNKNOWN_R 553536955		// Derecha
+#define UNKNOWN_S 3622325019	// Alto
 
-////////// IR REMOTE CODES //////////
-#define F 16736925	// FORWARD
-#define B 16754775	// BACK
-#define L 16720605	// LEFT
-#define R 16761405	// RIGHT
-#define S 16712445	// STOP
-#define UNKNOWN_F 5316027		  // FORWARD
-#define UNKNOWN_B 2747854299	// BACK
-#define UNKNOWN_L 1386468383	// LEFT
-#define UNKNOWN_R 553536955		// RIGHT
-#define UNKNOWN_S 3622325019	// STOP
-#define KEY1 16738455
-#define KEY2 16750695
-#define KEY3 16756815
-#define KEY4 16724175
-#define KEY5 16718055
-#define KEY6 16743045
-#define KEY7 16716015
-#define KEY8 16726215
-#define KEY9 16734885
-#define KEY0 16730805
-#define KEY_STAR 16728765
-#define KEY_HASH 16732845
+#define KEY_1 16738455        // 1
+#define KEY_2 16750695        // 2
+#define KEY_3 16756815        // 3
+#define KEY_4 16724175        // 4
+#define KEY_5 16718055        // 5
+#define KEY_6 16743045        // 6
+#define KEY_7 16716015        // 7
+#define KEY_8 16726215        // 8
+#define KEY_9 16734885        // 9
+#define KEY_0 16730805        // 0
+#define KEY_STAR 16728765     // *
+#define KEY_HASH 16732845     // #
 
-#define RECV_PIN  12
+/******************************ASIGNACION DE PINES************************/
+// Pines de motores de desplazamiento
+#define ENABLE_MOTOR_IZQ_PIN 5    // Potencia motor izquierdo
+#define ENABLE_MOTOR_DER_PIN 6	  // Potencia motor derecho
+#define MOTOR_IZQ_ADELA_PIN 7     // Motor izquierdo adelante
+#define MOTOR_IZQ_ATRAS_PIN 8	    // Motor izquierdo atras
+#define MOTOR_DER_ATRAS_PIN 9	    // Motor derecho atras
+#define MOTOR_DER_ADELA_PIN 11	  // Motor derecho adelante
 
-/*define channel enable output pins*/
-#define ENA 5	  // Left  wheel speed
-#define ENB 6	  // Right wheel speed
-/*define logic control output pins*/
-#define IN1 7	  // Left  wheel forward
-#define IN2 8	  // Left  wheel reverse
-#define IN3 9	  // Right wheel reverse
-#define IN4 11	// Right wheel forward
-#define carSpeed 250	// initial speed of car >=0 to <=255
+// Pin del infrarrojo
+#define INFRARROJO_PIN 12         // Infrarrojo
 
-int rightDistance = 0;
-int leftDistance = 0;
-int middleDistance = 0;
+// Pines del sensor ultrasonico
+#define ULTRASONIC_ECHO_PIN A4    // Echo
+#define ULTRASONIC_TRIG_PIN A5    // Trig
 
-IRrecv irrecv(RECV_PIN);
-decode_results results;
-unsigned long val;
-unsigned long preMillis;
+// Pin del servomotor
+#define SERVO_PIN 3               // Servomotor
 
-/**
- * BEGIN DEFINE FUNCTIONS
- */
+/************************************VARIABLES****************************/
+// Variables motores
+const int velocidadMaxima = 255;
 
-void forward(){ 
-  digitalWrite(ENA,HIGH);
-  digitalWrite(ENB,HIGH);
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,HIGH);
+// Variables infrarrojo
+IRrecv infrarrojo(INFRARROJO_PIN);
+decode_results resultadoDecodificado;
+unsigned long valorDecodificado;
+unsigned long milisegundosTiempo;
+
+// Variables sensor ultrasonico
+int distanciaDerecha = 0;
+int distanciaIzquierda = 0;
+int distanciaMedia = 0;
+
+// Variables servomotor
+Servo servo;
+
+/************************************FUNCIONES****************************/
+// Funciones para el movimiento del robot
+void adelante(int velocidad){ 
+  digitalWrite(MOTOR_IZQ_ADELA_PIN,HIGH);
+  digitalWrite(MOTOR_IZQ_ATRAS_PIN,LOW);
+  digitalWrite(MOTOR_DER_ATRAS_PIN,LOW);
+  digitalWrite(MOTOR_DER_ADELA_PIN,HIGH);
+  analogWrite(ENABLE_MOTOR_IZQ_PIN,velocidad);
+  analogWrite(ENABLE_MOTOR_DER_PIN,velocidad);
   Serial.println("go forward!");
 }
-void back(){
-  digitalWrite(ENA,HIGH);
-  digitalWrite(ENB,HIGH);
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,HIGH);
-  digitalWrite(IN3,HIGH);
-  digitalWrite(IN4,LOW);
+void atras(int velocidad){
+  digitalWrite(MOTOR_IZQ_ADELA_PIN,LOW);
+  digitalWrite(MOTOR_IZQ_ATRAS_PIN,HIGH);
+  digitalWrite(MOTOR_DER_ATRAS_PIN,HIGH);
+  digitalWrite(MOTOR_DER_ADELA_PIN,LOW);
+  analogWrite(ENABLE_MOTOR_IZQ_PIN,velocidad);
+  analogWrite(ENABLE_MOTOR_DER_PIN,velocidad);
   Serial.println("go back!");
 }
-void left(){
-  analogWrite(ENA,carSpeed);
-  analogWrite(ENB,carSpeed);
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,HIGH);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,HIGH); 
+void izquierda(int velocidad){
+  digitalWrite(MOTOR_IZQ_ADELA_PIN,LOW);
+  digitalWrite(MOTOR_IZQ_ATRAS_PIN,HIGH);
+  digitalWrite(MOTOR_DER_ATRAS_PIN,LOW);
+  digitalWrite(MOTOR_DER_ADELA_PIN,HIGH);
+  analogWrite(ENABLE_MOTOR_IZQ_PIN,velocidad);
+  analogWrite(ENABLE_MOTOR_DER_PIN,velocidad); 
   Serial.println("go left!");
 }
-void right(){
-  analogWrite(ENA,carSpeed);
-  analogWrite(ENB,carSpeed);
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW);
-  digitalWrite(IN3,HIGH);
-  digitalWrite(IN4,LOW);
+void derecha(int velocidad){
+  digitalWrite(MOTOR_IZQ_ADELA_PIN,HIGH);
+  digitalWrite(MOTOR_IZQ_ATRAS_PIN,LOW);
+  digitalWrite(MOTOR_DER_ATRAS_PIN,HIGH);
+  digitalWrite(MOTOR_DER_ADELA_PIN,LOW);
+  analogWrite(ENABLE_MOTOR_IZQ_PIN,velocidad);
+  analogWrite(ENABLE_MOTOR_DER_PIN,velocidad);
   Serial.println("go right!");
 }
-void stop(){
-  digitalWrite(ENA, LOW);
-  digitalWrite(ENB, LOW);
+void alto(){
+  digitalWrite(ENABLE_MOTOR_IZQ_PIN, LOW);
+  digitalWrite(ENABLE_MOTOR_DER_PIN, LOW);
   Serial.println("STOP!");  
 }
 
 //Ultrasonic distance measurement Sub function
-int Distance_test() {
-  digitalWrite(Trig, LOW);   
+int medirDistancia() {
+  digitalWrite(ULTRASONIC_TRIG_PIN, LOW);   
   delayMicroseconds(2);
-  digitalWrite(Trig, HIGH);  
+  digitalWrite(ULTRASONIC_TRIG_PIN, HIGH);  
   delayMicroseconds(20);
-  digitalWrite(Trig, LOW);   
-  float Fdistance = pulseIn(Echo, HIGH);  
-  Fdistance= Fdistance / 58;       
-  return (int)Fdistance;
+  digitalWrite(ULTRASONIC_TRIG_PIN, LOW);   
+  float distancia = pulseIn(ULTRASONIC_ECHO_PIN, HIGH);  
+  distancia = distancia / 58;       
+  return (int)distancia;
 }
 
-void ServoControl(uint8_t angleSetting)
+void controlServo(uint8_t angulo)
 {
-  if (angleSetting > 175)
+  if (angulo > 175)
   {
-    angleSetting = 175;
+    angulo = 175;
   }
-  else if (angleSetting < 5)
+  else if (angulo < 5)
   {
-    angleSetting = 5;
+    angulo = 5;
   }
-  myservo.attach(3);
-  myservo.write(angleSetting); //sets the servo position according to the  value
-  delay(500);
-  myservo.detach();
+  servo.attach(SERVO_PIN);
+  servo.write(angulo); //sets the servo position according to the  value
+  //delay(500);
+  servo.detach();
 }
 
 void evasorObstaculos() { 
-    myservo.write(90);  //setservo position according to scaled value
+    servo.write(90);  //setservo position according to scaled value
     delay(500); 
-    middleDistance = Distance_test();
+    distanciaMedia = medirDistancia();
 
-    if(middleDistance <= 40) {     
-      stop();
+    if(distanciaMedia <= 40) {     
+      alto();
       delay(500);                         
-      myservo.write(10);          
+      servo.write(10);          
       delay(1000);      
-      rightDistance = Distance_test();
+      distanciaDerecha = medirDistancia();
       
       delay(500);
-      myservo.write(90);              
+      servo.write(90);              
       delay(1000);                                                  
-      myservo.write(180);              
+      servo.write(180);              
       delay(1000); 
-      leftDistance = Distance_test();
+      distanciaIzquierda = medirDistancia();
       
       delay(500);
-      myservo.write(90);              
+      servo.write(90);              
       delay(1000);
-      if(rightDistance > leftDistance) {
-        right();
+      if(distanciaDerecha > distanciaIzquierda) {
+        derecha(velocidadMaxima);
         delay(360);
       }
-      else if(rightDistance < leftDistance) {
-        left();
+      else if(distanciaDerecha < distanciaIzquierda) {
+        izquierda(velocidadMaxima);
         delay(360);
       }
-      else if((rightDistance <= 40) || (leftDistance <= 40)) {
-        back();
+      else if((distanciaDerecha <= 40) || (distanciaIzquierda <= 40)) {
+        atras(velocidadMaxima);
         delay(180);
       }
       else {
-        forward();
+        adelante(velocidadMaxima);
       }
     }  
     else {
-        forward();
+        adelante(velocidadMaxima);
     }                     
 }
 
 void setup() {
   Serial.begin(9600);
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  pinMode(IN3,OUTPUT);
-  pinMode(IN4,OUTPUT);
-  pinMode(ENA,OUTPUT);
-  pinMode(ENB,OUTPUT);
-  pinMode(Echo, INPUT);    
-  pinMode(Trig, OUTPUT);
-  //myservo.attach(3,700,2400);
-  stop();
-  irrecv.enableIRIn();  
+  pinMode(MOTOR_IZQ_ADELA_PIN,OUTPUT);
+  pinMode(MOTOR_IZQ_ATRAS_PIN,OUTPUT);
+  pinMode(MOTOR_DER_ATRAS_PIN,OUTPUT);
+  pinMode(MOTOR_DER_ADELA_PIN,OUTPUT);
+  pinMode(ENABLE_MOTOR_IZQ_PIN,OUTPUT);
+  pinMode(ENABLE_MOTOR_DER_PIN,OUTPUT);
+  pinMode(ULTRASONIC_ECHO_PIN, INPUT);    
+  pinMode(ULTRASONIC_TRIG_PIN, OUTPUT);
+  //servo.attach(3,700,2400);
+  alto();
+  infrarrojo.enableIRIn();  
 }
 
 void loop() {
-  if (irrecv.decode(&results)){ 
-    preMillis = millis();
-    val = results.value;
-    Serial.println(val);
-    irrecv.resume();
-    switch(val){
+  if (infrarrojo.decode(&resultadoDecodificado)){ 
+    milisegundosTiempo = millis();
+    valorDecodificado = resultadoDecodificado.value;
+    Serial.println(valorDecodificado);
+    infrarrojo.resume();
+    switch(valorDecodificado){
       case F: 
       case UNKNOWN_F: 
-        forward(); 
+        adelante(velocidadMaxima); 
         break;
       case B: 
       case UNKNOWN_B: 
-        back(); 
+        atras(velocidadMaxima); 
         break;
       case L: 
       case UNKNOWN_L: 
-        left(); 
+        izquierda(velocidadMaxima); 
         break;
       case R:
       case UNKNOWN_R: 
-        right();
+        derecha(velocidadMaxima);
         break;
       case S: 
       case UNKNOWN_S: 
-        stop(); 
+        alto(); 
         break;
       //case KEY1: 
       //case 4294967295: evasorObstaculos(); break;
@@ -217,9 +229,9 @@ void loop() {
     }
   }
   else{
-    if(millis() - preMillis > 500){
-      stop();
-      preMillis = millis();
+    if(millis() - milisegundosTiempo > 500){
+      alto();
+      milisegundosTiempo = millis();
     }
   }
 } 
